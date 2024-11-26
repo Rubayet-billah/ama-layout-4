a48bcc1535b12ccbb74a737efaa110ed019075b7
Rubayet-billah <rubayetbillah909@gmail.com>
2024-11-27 02:50:51 +0600

    section title added

--- components/home/Service.js
+++ components/home/Service.js
@@ -5,6 +5,7 @@ import icon1 from "@/assets/serviceCard/servicesIcon/Picture1.png";
 import icon2 from "@/assets/serviceCard/servicesIcon/Picture2.png";
 import icon3 from "@/assets/serviceCard/servicesIcon/Picture3.png";
 import artWorkUnderline from "@/assets/artWorks/artWorkUnderline.png";
+import artWork1 from "@/assets/bannerSlider/artwork1.png";
 import image1 from "@/assets/serviceCard/image-1.jpg";
 import image2 from "@/assets/serviceCard/image-2.jpeg";
 import image3 from "@/assets/serviceCard/image-3.jpeg";
@@ -39,23 +40,28 @@ const Service = () => {
   ];
 
   return (
-    <CustomContainer classNames="py-10">
-      <header className="mb-10 lg:mb-16">
-        <div className="relative w-fit">
-          <span className="font-bold mrf-secondary-heading">
-            Market Research By Category
-          </span>
-          <div className="absolute w-56 -bottom-4 lg:w-80 -right-16 ">
-            <MRFImage src={artWorkUnderline} alt="artwork underline" />
+    <div className="bg-tertiary">
+      <CustomContainer classNames="py-10">
+        <header className="my-10 lg:my-16">
+          <div className="relative mx-auto w-fit">
+            <span className="absolute w-20 -left-5 -top-10 lg:w-28">
+              <MRFImage src={artWork1} alt="artwork 1" />
+            </span>
+            <span className="font-bold mrf-secondary-heading">
+              Core Services
+            </span>
+            <div className="absolute w-56 -bottom-4 lg:w-80 -right-16 ">
+              <MRFImage src={artWorkUnderline} alt="artwork underline" />
+            </div>
           </div>
-        </div>
-      </header>
-      <section className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:gap-12">
-        {services.map((service) => (
-          <ServiceCard key={service.id} service={service} />
-        ))}
-      </section>
-    </CustomContainer>
+        </header>
+        <section className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:gap-12">
+          {services.map((service) => (
+            <ServiceCard key={service.id} service={service} />
+          ))}
+        </section>
+      </CustomContainer>
+    </div>
   );
 };
 
