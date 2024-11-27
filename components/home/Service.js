608a35488a7da4cf03278a4a197adb48415abbc4
Rubayet-billah <rubayetbillah909@gmail.com>
2024-11-27 19:42:36 +0600

    service section added

--- components/home/Service.js
+++ components/home/Service.js
@@ -5,11 +5,14 @@ import icon1 from "@/assets/serviceCard/servicesIcon/Picture1.png";
 import icon2 from "@/assets/serviceCard/servicesIcon/Picture2.png";
 import icon3 from "@/assets/serviceCard/servicesIcon/Picture3.png";
 import artWorkUnderline from "@/assets/artWorks/artWorkUnderline.png";
-import artWork1 from "@/assets/bannerSlider/artwork1.png";
+import artWorkSpiral from "@/assets/artWorks/artWorkSpiral.png";
+import waveBg from "@/assets/artWorks/waveBg.png";
 import image1 from "@/assets/serviceCard/image-1.jpg";
 import image2 from "@/assets/serviceCard/image-2.jpeg";
 import image3 from "@/assets/serviceCard/image-3.jpeg";
 import MRFImage from "../ui/Image";
+import Button from "../ui/Button";
+import { ArrowRight } from "react-feather";
 
 const Service = () => {
   const services = [
@@ -17,7 +20,7 @@ const Service = () => {
       id: 1,
       title: "Consulting",
       description:
-        "Get expert advice and strategic guidance tailored to your specific business needs.",
+        "Gain access to expert advice and strategic guidance tailored precisely to your business needs. Our seasoned consultants provide insightful solutions and actionable recommendations ",
       icon: icon1,
       image: image1,
     },
@@ -40,17 +43,20 @@ const Service = () => {
   ];
 
   return (
-    <div className="bg-tertiary">
-      <CustomContainer classNames="py-10">
+    <div className="relative">
+      <div className="absolute w-full h-full">
+        <MRFImage src={waveBg} alt="wave bg" classNames="h-full" />
+      </div>
+      <CustomContainer classNames="py-10 relative z-[1]">
         <header className="my-10 lg:my-16">
           <div className="relative mx-auto w-fit">
             <span className="absolute w-20 -left-5 -top-10 lg:w-28">
-              <MRFImage src={artWork1} alt="artwork 1" />
+              <MRFImage src={artWorkSpiral} alt="artwork spiral" />
             </span>
             <span className="font-bold mrf-secondary-heading">
               Core Services
             </span>
-            <div className="absolute w-56 -bottom-4 lg:w-80 -right-16 ">
+            <div className="absolute w-32 -bottom-2 lg:w-56 -right-16 ">
               <MRFImage src={artWorkUnderline} alt="artwork underline" />
             </div>
           </div>
@@ -60,6 +66,12 @@ const Service = () => {
             <ServiceCard key={service.id} service={service} />
           ))}
         </section>
+
+        <div className="grid mt-10 place-items-center lg:mt-16 lg:mb-32">
+          <Button variant="secondary" classNames="rounded-full">
+            Explore More <ArrowRight size={18} />
+          </Button>
+        </div>
       </CustomContainer>
     </div>
   );
--- components/home/service/ServiceCard.js
+++ components/home/service/ServiceCard.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import MRFImage from "@/components/ui/Image";
+import { ArrowRight } from "react-feather";
+import Button from "@/components/ui/Button";
 
 const ServiceCard = ({ service }) => {
   const [isHovered, setIsHoverd] = useState(false);
@@ -10,31 +12,20 @@ const ServiceCard = ({ service }) => {
   const { title, description, image, icon } = service;
 
   return (
-    <div
-      onMouseEnter={(e) => setIsHoverd(true)}
-      onMouseLeave={(e) => setIsHoverd(false)}
-      className="relative overflow-hidden transition duration-300 shadow-lg hover:shadow-xl"
-    >
-      {/* Background image */}
-      <div
-        className="h-64 bg-center bg-cover"
-        // style={{ backgroundImage: `url(${image.src})` }}
-      >
-        <MRFImage classNames="w-full" src={image} alt={title + " - Thumb"} />
-      </div>
-      <div
-        className={`absolute left-0 right-0 h-full px-4 py-2 text-white transition duration-300 opacity-100 bg-opacity-80 bg-primary top-[200px] backdrop-filter ${
-          isHovered && "-translate-y-[200px]"
-        }`}
-      >
-        <div className="flex flex-col items-start gap-2">
+    <div>
+      <div className={`px-6 py-4 bg-white h-full hover:shadow-xl rounded-xl`}>
+        <div className="flex flex-col items-start h-full gap-2">
           <h3 className="flex items-center gap-3 text-xl font-bold">
-            <Image className="w-10 h-10" src={icon} alt="icon" />
+            <div className="p-2 rounded-full bg-neutral">
+              <Image className="w-8 h-8" src={icon} alt="icon" />
+            </div>
             {title}
           </h3>
-          <div className="px-4 text-sm text-accent">{description}</div>
-          <Link href="/service" className="mx-4 text-sm text-secondary">
-            Explore more
+          <div className="flex-1 mt-3 text-sm">{description}</div>
+          <Link href="/service" className="text-sm text-secondary">
+            <Button classNames="px-0 bg-transparent text-secondary hover:bg-tranparent hover:text-primary hover:shadow-none">
+              Explore more <ArrowRight size={18} />
+            </Button>
           </Link>
         </div>
       </div>
